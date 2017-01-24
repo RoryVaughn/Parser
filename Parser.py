a3042f34dbbf3172ca79c5bb2f3c29240fdd744a
Rory Vaughn <Rvaughn2525@gmail.com>
2017-01-24 13:54:37 -0600

    print format setup

--- Parser.py
+++ Parser.py
@@ -1,12 +1,12 @@
 f = open("CNF.txt", "r")
 run = 0
 a = 0
-b = 0
+b = 1
 c = 0
 d = 0
 e = 0
 g = 0
-h = 0
+h = 1
 true = "1"
 false = "0"
 while run != 5:
@@ -16,6 +16,7 @@ while run != 5:
     literals = 0
     solve = []
     current = []
+    fitness = 0
     names = []
     for i in range (0,len(text)):
         if text[i]=="(":
@@ -24,7 +25,7 @@ while run != 5:
             current = []
             solve = []
             count = 0
-        
+
         if text[i] != ")":
             current.append(text[i])
             if text[i - 1] == "!":
@@ -35,38 +36,32 @@ while run != 5:
             else:
                 if text[i].isalpha():
                     solve.append(text[i])
-
                     
-                    
-                
-            
         else:
             current.append(text[i])
+            print ''.join(current)
             for x in range (0,len(solve)):
                 if solve[x].isalpha():
                     count += 1
             if eval(" + ".join(solve)) >= 1:
                 clauseSolve = "true"
+                fitness += 1
             else:
                 clauseSolve = "false"
             print "This clause is", clauseSolve
-
             print "number of true literals -", eval (" + ".join(solve))
-            
-            print ''.join(current)
             print " "
-            
-                
-            
+
         if text[i].isalpha() and text[i] not in names:
             names.append( text[i])
             literals += 1
-            
 
-    
     print(text)
+    print "The fitness is",fitness, "out of",clauses, "for this expression."
+    if fitness == clauses:
+        print "This expression is solved"
     print "The number of clauses is ",clauses
     print "The number of literals is ",literals
     print "The used literals are ",names
-    print "__________________________________"
-	
+    print "_________________________________________________________________"
+    print "_________________________________________________________________"
